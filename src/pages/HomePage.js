0be3b75e813db31c45891a232e1a846f74e344a7
sharonay2015 <youngs_e@hotmail.com>
2019-12-11 22:04:27 -0500

    beginning files for react app initialized

---
+++ src/pages/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+
+    <h1>Hello, welcome to my blog!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis egestas semper. Aliquam lorem ex, consectetur eu diam et, lacinia finibus dui. Nulla iaculis justo ut mauris gravida sodales. Mauris pulvinar, nibh vitae feugiat gravida, orci lectus malesuada neque, eget pretium urna nisi non eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec id nibh tellus. Vestibulum eu volutpat dolor. Proin faucibus turpis ac varius molestie. Ut maximus ut orci a egestas.
+    </p>
+
+    <p>
+      Ut vestibulum ut dui vel iaculis. Ut sagittis ligula et mauris sodales, nec iaculis nisl accumsan. Aenean vel mollis quam. Suspendisse ultrices non odio eu dignissim. Vestibulum fermentum finibus eros a ornare. Praesent lobortis dignissim massa sed volutpat. Curabitur tincidunt pulvinar tortor, imperdiet ullamcorper turpis convallis at. Pellentesque lacinia vehicula augue ac fringilla. Nunc commodo risus ligula, quis vestibulum dui semper vitae. Vivamus id euismod mauris.
+    </p>
+
+    <p>
+      Curabitur in dolor ac nulla elementum faucibus sit amet eget libero. Morbi vel cursus tellus, ut molestie dolor. Vivamus lobortis ipsum nunc, ut mollis sem mattis sit amet. Etiam in tempor orci. Mauris vehicula suscipit est, eu pharetra lectus consectetur et. Quisque feugiat eros magna, id ultrices urna tincidunt nec. Pellentesque egestas ac sapien a pulvinar. Duis vel rhoncus justo.
+    </p>
+  </>
+);
+
+export default HomePage;
