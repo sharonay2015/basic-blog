dd5b2e4a360bc81bdc30da9b6a566442f8fd850a
sharonay2015 <youngs_e@hotmail.com>
2019-12-11 23:40:35 -0500

    added functionality to article page routes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
             <Route path="/" component={HomePage} exact />
             <Route path="/about" component={AboutPage} />
             <Route path="/article-list" component={ArticleList} />
-            <Route path="/article" component={ArticlePage} />
+            <Route path="/article/:name" component={ArticlePage} />
           </div>
         </div>
       </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-  <>
-    <h1>This is an article</h1>
-  </>
-);
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  // find article from articles array that has the name that matches the name in the url
+  const article = articleContent.find(article => article.name === name);
+
+  if (!article) return <h1>Article does not exist</h1>
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
+  );
+}
 
 export default ArticlePage;
