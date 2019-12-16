24839f591b681c6330aae71545e0d07c225091f5
sharonay2015 <youngs_e@hotmail.com>
2019-12-15 22:25:27 -0500

    added some styling

--- basic-blog-frontend/src/Navbar.js
+++ basic-blog-frontend/src/Navbar.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => (
   <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
+
+    <ul class="navbar" role="navigation">
+      <li class="left">
         <Link to="/about">About</Link>
       </li>
       <li>
         <Link to="/article-list">Articles</Link>
       </li>
+
+      <li class="home social"><Link to="/"><i class="fa fa-home"></i></Link></li>
+      <li class="social"><a href="http://www.twitter.com"> <i class="fa fa-twitter"></i></a></li>
+      <li class="social"><a href="http://www.twitter.com" > <i class="fa fa-facebook"></i></a></li>
+      <li class="social"><a href="http://www.twitter.com" > <i class="fa fa-github"></i></a></li>
     </ul>
   </nav>
 );
