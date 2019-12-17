01ad215e98603a7dd8b290fdaf83a5cd33ab894c
sharonay2015 <youngs_e@hotmail.com>
2019-12-17 00:42:50 -0500

    installed nodemon and tested endpoints using postman

--- basic-blog-backend/src/server.js
+++ basic-blog-backend/src/server.js
@@ -4,12 +4,15 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
   'learn-react': {
     upvotes: 0,
+    comments: [],
   },
   'learn-node': {
     upvotes: 0,
+    comments: [],
   },
   'learn-budgeting': {
     upvotes: 0,
+    comments: [],
   },
 }
 
@@ -24,8 +27,17 @@ app.post('/api/articles/:name/upvote', (req, res) => {
   const articleName = req.params.name;
 
   articlesInfo[articleName].upvotes += 1;
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
-})
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!!!`)
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({ username, text });
+
+  res.status(200).send(articlesInfo[articleName]);
+});
 
 // start server
 app.listen(8000, () => console.log('Listening on port 8000'));
