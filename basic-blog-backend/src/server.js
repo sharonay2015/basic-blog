70859723b9eac8e0048373402529a4a233112603
sharonay2015 <youngs_e@hotmail.com>
2019-12-17 00:14:46 -0500

    tested node server, working. GET and POST successful.

--- basic-blog-backend/src/server.js
+++ basic-blog-backend/src/server.js
@@ -1,10 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
+const articlesInfo = {
+  'learn-react': {
+    upvotes: 0,
+  },
+  'learn-node': {
+    upvotes: 0,
+  },
+  'learn-budgeting': {
+    upvotes: 0,
+  },
+}
 
 // create backend app
 const app = express();
 
+// parse json object, adds body property to request parameter t
+app.use(bodyParser.json());
+
 // define endpoints for app
-app.get('/hello', (req, res) => res.send('Hey Yall!!'));
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].upvotes += 1;
+  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+})
 
 // start server
 app.listen(8000, () => console.log('Listening on port 8000'));
