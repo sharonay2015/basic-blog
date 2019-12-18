f3e3a2e1b4dbb463d202f057a20f568f9093259d
sharonay2015 <youngs_e@hotmail.com>
2019-12-18 00:13:43 -0500

    setup mongodb, created a connect for local db

--- basic-blog-backend/src/server.js
+++ basic-blog-backend/src/server.js
@@ -1,26 +1,34 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-  'learn-react': {
-    upvotes: 0,
-    comments: [],
-  },
-  'learn-node': {
-    upvotes: 0,
-    comments: [],
-  },
-  'learn-budgeting': {
-    upvotes: 0,
-    comments: [],
-  },
-}
+import { MongoClient } from 'mongodb';
 
 // create backend app
 const app = express();
 
 // parse json object, adds body property to request parameter t
 app.use(bodyParser.json());
+
+// callback
+app.get('/api/articles/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    // connect to local database, return promise
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+    // specify database to query 
+    const db = client.db('basic-blog');
+
+    // query db, 
+    const articleInfo = await db.collection('articles').findOne({ name: articleName })
+    // response from the db
+    res.status(200).json(articleInfo);
+
+    // close db connection
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error });
+  }
+})
 
 // define endpoints for app
 app.post('/api/articles/:name/upvote', (req, res) => {
@@ -35,7 +43,6 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
   const articleName = req.params.name;
 
   articlesInfo[articleName].comments.push({ username, text });
-
   res.status(200).send(articlesInfo[articleName]);
 });
 
