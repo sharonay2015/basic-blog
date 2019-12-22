e008c0ef9f6aa20afe28d8c86110483d6a2ae380
sharonay2015 <youngs_e@hotmail.com>
2019-12-22 11:42:16 -0500

    added hooks to keep track of state and load article info

--- basic-blog-frontend/src/pages/ArticlePage.js
+++ basic-blog-frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticleList from '../components/ArticleList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
@@ -8,6 +8,15 @@ const ArticlePage = ({ match }) => {
   // find article from articles array that has the name that matches the name in the url
   const article = articleContent.find(article => article.name === name);
 
+  // hook to keep track of state
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+  // test useEffect
+  useEffect(() => {
+    setArticleInfo({ upvotes: 3 })
+  })
+
+  // hook to load article info
+
   if (!article) return <NotFoundPage />
 
   // include all the articles in the list except the one currently selected 
@@ -15,6 +24,7 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times </p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
